31a7964a0ccbf35f36d85df35f879e14e438ff3a
Paul <ing12xunder@gmail.com>
2023-04-19 20:16:50 -0500

    Modificando libros

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,16 +10,40 @@ import { NgOptimizedImage } from '@angular/common'
 })
 export class AppComponent {
   title = 'Biblioteca';
-  Estante: Libros;
+  Libro1: Libros;Libro2: Libros;Libro3: Libros;
   aleatorio:Number = Math.floor(Math.random() * 100);
   tiempo:Date = new Date(Date.UTC(2023, 4, 18));
 
   constructor(){
-    this.Estante = {
+    this.Libro1 = {
         id: 1,
         Titulo: 'La ciudad y los perros',
         Autor: 'Mario Vargas Llosa',
         Fecha: '1963',
-      }
+      },
+    this.Libro2 = {
+      id: 2,
+      Titulo: 'Travesuras de la niña mala',
+      Autor: 'Mario Vargas Llosa',
+      Fecha: '2006',
+    },
+    this.Libro3 = {
+      id: 3,
+      Titulo: 'Los cachorros',
+      Autor: 'Mario Vargas Llosa',
+      Fecha: '1967',
+    }
+    /* const libro1: Libros = {
+      id: 1,
+      Titulo: 'La ciudad y los perros',
+      Autor: 'Mario Vargas Llosa',
+      Fecha: '1963',
+    };
+    const libro2: Libros = {
+      id: 2,
+      Titulo: 'La ciudad y los perros',
+      Autor: 'Mario Vargas Llosa',
+      Fecha: '1963',
+    }; */
   }
 }
